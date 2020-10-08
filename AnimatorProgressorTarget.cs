a5b841296fea1f3d845563427fc646d250a96942
fatihkran34 <70379046+fatihkran34@users.noreply.github.com>
2020-10-08 18:16:35 +0300

    V 1.0

---
+++ AnimatorProgressorTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LCUI {
+    public class AnimatorProgressorTarget : ProgressorTarget
+    {
+        [SerializeField]private Animator animator;
+        [SerializeField] private string animatorParameter;
+
+        private int hash;
+
+        private void Awake()
+        {
+            hash = Animator.StringToHash(animatorParameter);
+            if (animator==null)
+            {
+                animator = GetComponent<Animator>();
+            }
+        }
+
+        public override void SetValue(float value)
+        {
+            animator.SetFloat(hash, value);
+        }
+    }
+}
---
+++ ProgressorTarget.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LCUI
+{
+    public abstract class ProgressorTarget : MonoBehaviour
+    {
+        public abstract void SetValue(float value);
+    }
+}
